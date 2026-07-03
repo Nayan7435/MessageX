8feaeb2bbb8d9daff258f918aa26b1786497b9c0
Gohel Nayan <gohelnayan264-gmail@gmail.com>
2026-07-03 14:09:04 +0530

    feat: initial server setup with Express, Socket.io, and MongoDB connection

---
+++ server/server.js
@@ -0,0 +1,39 @@
+require('dotenv').config({ path: require('path').resolve(__dirname, '.env') });
+const express = require('express');
+const cors = require('cors');
+const connectDB = require('./config/db');
+const http = require('http');
+const { Server } = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+
+const io = new Server(server, {
+    cors: {
+        origin: 'http://localhost:5173',
+        methods: ['GET', 'POST'],
+    },
+});
+
+console.log('MONGO_URI:', process.env.MONGO_URI);
+connectDB();
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('MessageX API is running');
+});
+
+io.on('connection', (socket) => {
+    console.log('User Connected:', socket.id);
+
+    socket.on('disconnect', () => {
+        console.log('User disconnected:', socket.id);
+    });
+});
+
+const PORT = process.env.PORT || 5001;
+server.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
